b7e4311366203a5ed76e0d8b5507dde74ef0e195
Alex Cooper <alexander.cooper@monash.edu>
2018-07-19 13:25:43 +1000

    Transforms for manipulating random matrices into unrestricted random vectors.

---
+++ ptvi/transform.py
@@ -0,0 +1,64 @@
+"""Pytorch-differentiable functions for reshaping and transforming data.
+
+The main use of these transforms is to allow useful random objects (eg random
+psd matrices) to be represented as random vectors that can be optimized.
+"""
+
+import torch
+import math
+import numpy as np
+
+
+def vec_to_tril(l):
+    assert l.ndimension() == 1, 'l should have dim=1'
+    # lower triangle has k(k+1)//2 entries
+    k = round(-1 + math.sqrt(1 + 8 * len(l))) // 2
+    L = torch.zeros((k, k))
+    L[np.tril_indices(k)] = l
+    return L
+
+
+def tril_to_vec(L):
+    """Convert a lower-triangular matrix L to a vector l."""
+    assert L.ndimension() == 2, 'L should have dimension 2'
+    return L[np.tril_indices(L.shape[0])]
+
+
+# this function is differentiable as-is
+def vec_to_trilpd(l):
+    """Convert a vector ls to a lower-triangular matrix L and exponentiate the
+    diagonal entries."""
+    assert l.ndimension() == 1, 'l should have dimension 1'
+    # lower triangle has k(k+1)//2 entries
+    k = round(-1 + math.sqrt(1 + 8 * len(l))) // 2
+    L = torch.zeros((k, k))
+    L[np.tril_indices(k)] = l
+    L[np.diag_indices(k)] = torch.exp(L[np.diag_indices(k)])
+    return L
+
+
+def trilpd_to_vec(L):
+    """Convert a lower triangular factor with positive diagonal to a vector,
+    after first taking elementwise logs of the diagonals. Autodiffable.
+    """
+    k = L.shape[0]
+    assert L.shape == (k, k) and all(torch.diag(L) > 0)
+    L2 = L * (1 - torch.eye(k)) + torch.diag(torch.log(torch.diag(L)))
+    return L2[np.tril_indices(k)]
+
+
+def pd_to_vec(A):
+    """Convert a positive-definite matrix A to a vector l of entries from
+    its cholesky factor. Diagonal entries are logged so they occupy the full
+    real line, and still map back to positive values.
+    """
+    L = torch.potrf(A, upper=False)
+    return trilpd_to_vec(L)
+
+
+def vec_to_pd(l):
+    """Convert a vector created by pd_to_vec back to a positive-definite matrix.
+    """
+    L = vec_to_trilpd(l)
+    return torch.matmul(L, L.t())
+
---
+++ tests/test_transform.py
@@ -0,0 +1,66 @@
+import unittest
+from ptvi.transform import (tril_to_vec, vec_to_tril, trilpd_to_vec,
+                            vec_to_trilpd, pd_to_vec, vec_to_pd)
+import torch
+from torch.autograd import gradcheck
+import numpy as np
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_tril_to_vec(self):
+        l = torch.tensor([1., 2., 3., 4., 5., 6.])  # for a 3x3 matrix
+        L = vec_to_tril(l)
+        l2 = tril_to_vec(L)
+        L2 = torch.tensor([[1., 0, 0], [2., 3., 0], [4., 5., 6.]])
+        self.assertTrue(torch.allclose(l, l2))
+        self.assertTrue(torch.allclose(L, L2))
+        # 1x1 edge case
+        self.assertTrue(torch.allclose(
+            torch.tensor([1.]), tril_to_vec(vec_to_tril(torch.tensor([1.])))))
+        self.assertTrue(torch.tensor([[1.]]) ==
+                        vec_to_tril(tril_to_vec(torch.tensor([[1.]]))))
+
+    def test_vec_to_tril_gradients(self):
+        def f(x):
+            return vec_to_tril(torch.tensor([0, x, x**2]))  # gradients 0, 1, 2x
+        gradcheck(f, (torch.tensor(2.),))
+
+    def test_tril_to_vec_gradients(self):
+        def f(x):
+            return tril_to_vec(torch.tensor([[0, 0], [x, x**2]]))  # 0, 1, 2x
+        gradcheck(f, (torch.tensor(3.),))
+
+    def test_vec_to_trilpd_and_back(self):
+        # for a 3x3 matrix
+        l = torch.tensor([0., 2., np.log(3.), 4., 5., np.log(6.)])
+        L = vec_to_trilpd(l)
+        l2 = trilpd_to_vec(L)
+        L2 = torch.tensor([[1., 0, 0], [2., 3., 0], [4., 5., 6.]])
+        self.assertTrue(torch.allclose(l, l2))
+        self.assertTrue(torch.allclose(L, L2))
+        # 1x1 edge case
+        self.assertTrue(torch.allclose(
+            torch.tensor([1.]),
+            trilpd_to_vec(vec_to_trilpd(torch.tensor([1.])))))
+        self.assertTrue(torch.allclose(
+            torch.tensor([[1.]]),
+            vec_to_trilpd(trilpd_to_vec(torch.tensor([[1.]])))))
+
+    def test_trilpd_gradients(self):
+        gradcheck(vec_to_trilpd, (torch.tensor([1., 2., 3.]),))
+        gradcheck(trilpd_to_vec,
+                  (torch.tensor([[0.5, 0.], [1, np.log(3.)]]),))
+
+        def f(x: float):
+            return trilpd_to_vec(torch.tensor([[x, 0.], [x**2, np.log(x**3)]]))
+        gradcheck(f, (1.5,))
+
+    def test_pd_vec_gradients(self):
+        torch.manual_seed(123)
+        gradcheck(vec_to_pd, (torch.tensor([1., 2., 3.]),))
+        L = torch.randn((4, 4))
+        A = L@L.t() + 5*torch.eye(4)
+        assert all(torch.symeig(A)[0] > 0)
+        # only defined for psd matrices, soo...
+        gradcheck(lambda B: pd_to_vec(B@B.t()), (A,))
