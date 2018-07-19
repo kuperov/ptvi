0eb0fc5d0d0fa1f447bc84e33c5903eba72f6cde
Alex Cooper <alex@acooper.org>
2018-07-20 09:22:43 +1000

    sketch of inverse wishart distribution

--- ptvi/dists.py
+++ ptvi/dists.py
@@ -1,5 +1,7 @@
+import math
+from scipy import stats, special
 import torch
-from torch.distributions import Transform
+from torch.distributions import Transform, constraints
 
 
 class Reciprocal(Transform):
@@ -21,36 +23,62 @@ def InvGamma(a, b):
 
 
 class InvWishart(torch.distributions.Distribution):
-    @property
-    def support(self):
-        pass
+    """Inverse-Wishart distribution, which is a distribution over real-valued
+    positive-definite matrices.
 
-    @property
-    def arg_constraints(self):
-        pass
+    """
+
+    arg_constraints = {'df': constraints.positive,
+                       'scale': constraints.positive_definite}
+    support = constraints.real
+    has_rsample = False
+
+    def __init__(self, df, scale):
+        """Create inverse Wishart distribution.
+
+        Args:
+            df:    degrees of freedom parameter
+            scale: positive-definite scale matrix
+        """
+        self.df, self.scale, self.p = df, scale, scale.shape[0]
 
     @property
     def mean(self):
-        pass
+        return self.scale/(self.df - self.p - 1)
 
-    @property
-    def variance(self):
-        pass
-
-    def rsample(self, sample_shape=torch.Size()):
-        pass
+    # @property
+    # def variance(self):
+    #     pass
+    # TODO: implement variance
 
     def log_prob(self, value):
-        pass
+        _p = self.p
+        assert value.shape == (_p, _p), f'value should be {_p}x{_p} psd'
+        X_inv = value.inverse()
+        _df = self.df
+        return (
+            + 0.5 * _df * torch.slogdet(self.scale)[1]
+            - 0.5 * _df * _p * math.log(2)
+            - special.multigammaln(0.5 * _df, _p)
+            - 0.5 * (_df + _p + 1) * torch.slogdet(value)[1]  # |X|^{-(nu+p+1)/2}
+            - 0.5 * torch.trace(self.scale @ X_inv)
+        )
 
-    def cdf(self, value):
-        pass
-
-    def icdf(self, value):
-        pass
-
-    def enumerate_support(self):
-        pass
-
-    def entropy(self):
-        pass
+# def logpdf_cholesky(L, df, Psi):
+#     """Inverse-wishart log density with Cholesky parameterization.
+#
+#     Computes p(X | Psi, df) where X ~ W^-1(Psi, df), where X = L@L.T and L is
+#     lower-triangular
+#
+#     Returns:
+#       log density of L
+#     """
+#     p = Psi.shape[0]
+#     L_inv = np.linalg.inv(np.tril(L))  # better to backsolve
+#     return (
+#             + 0.5 * df * np.linalg.slogdet(Psi)[1]
+#             - 0.5 * df * p * np.log(2)
+#             - special.multigammaln(0.5 * df, p)
+#             - (df + p + 1) * np.sum(np.log(np.diag(L)))  # |X|^{-(nu+p+1)/2}
+#             - 0.5 * np.trace(np.dot(np.dot(Psi, L_inv.T), L_inv))
+#     )
