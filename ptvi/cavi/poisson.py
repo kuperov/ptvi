bace070994017eeaefc6012c7b36d32ace4890fe
Alex Cooper <alexander.cooper@monash.edu>
2019-02-20 13:37:52 +1100

    Early work on Poisson procedure.

--- ptvi/cavi/poisson.py
+++ ptvi/cavi/poisson.py
@@ -6,7 +6,7 @@ See: [1] Arridge, S. R., Ito, K., Jin, B., & Zhang, C. (2018). Variational
 """
 
 import numpy as np
-from scipy import stats
+from scipy import stats, special
 from sklearn.decomposition import TruncatedSVD
 
 
@@ -33,7 +33,7 @@ def simulate(N, beta0, X=None):
     return (y, X)
 
 
-def poisson_vi_reg(y, X, tol=1e-5, maxiter=1_000):
+def poisson_vi_reg(y, A, mu_0, C_0, tol=1e-5, maxiter=1_000):
     """Fit a poisson regression using VGA using [1].
 
     This optimization uses one fixed-point update and one
@@ -41,27 +41,48 @@ def poisson_vi_reg(y, X, tol=1e-5, maxiter=1_000):
 
     Args:
         y:       response variable
-        X:       design matrix
+        A:       design matrix
+        mu_0:    prior mean
+        C_0:     prior variance
         tol:     convergence tolerance
         maxiter: maximum number of iterations
 
     Return:
         dict of various result values
     """
-    N, k, = X.shape
+    N, k, = A.shape
     assert y.shape == (N,)
     old_elbo = None
     beta = np.empty(k)
-    U, Sigma, V = np.linalg.svd(X, full_matrices=True)  # replace with rSVD
-    
+    U, Sigma, V = np.linalg.svd(A, full_matrices=True)  # replace with rSVD
+    C_0_inv = np.linalg.inv(C_0)
+    C, x_bar = C_0, mu_0  # initial value for C
+    m = C.shape[0]
+    elbo_const = (
+        - 0.5 * np.linalg.slogdet(C_0)[1] - 0.5 * m
+        - np.sum(special.gammaln(y + 1))
+    )
+
     def print_status():
         print(f'{i:4d}. elbo = {elbo}, beta = {beta.round(2)}')
 
     for i in range(maxiter):
-        G = ...
-        curlG = ...
-
-        elbo = -1
+        # Newton-Raphson update for x_bar
+        G = A.T @ np.exp(A@x_bar + 0.5 * np.diag(A @ C @ A.T)) + C_0_inv @ (x_bar - mu_0) - A.T@y
+        curlG = A.T @ np.diag(np.exp(A @ x_bar + 0.5 * A @ C @ A.T)) @ A + C_0_inv
+        delta_x = np.linalg.solve(curlG, -G)
+        x_bar = x_bar + delta_x
+        # fixed point update for C
+        D = np.diag(np.exp(A@x_bar + 0.5 * np.diag(A @ C @ A.T)))
+        C = np.linalg.inv(C_0_inv + A.T @ D @ A)
+        # elbo for monitoring convergence
+        elbo = (
+            y.T@A@x_bar - np.sum(np.exp(A@x_bar + 0.5 * np.diag(A@C@A.T))) 
+            - 0.5 * (x_bar - mu_0).T @ C_0_inv @ (x_bar - mu_0)
+            - 0.5 * np.trace(C_0_inv @ C)
+            + 0.5 * np.linalg.slogdet(C)[1]
+            + elbo_const
+        )
         if old_elbo and elbo - old_elbo < tol:
             print('Convergence detected.')
             break
--- ptvi/cavi/test_poisson.py
+++ ptvi/cavi/test_poisson.py
@@ -1,6 +1,7 @@
 import unittest
 from ptvi.cavi import poisson
 import numpy as np
+from statsmodels.discrete.discrete_model import Poisson
 
 
 class TestPoisson(unittest.TestCase):
@@ -10,9 +11,13 @@ class TestPoisson(unittest.TestCase):
         y, X = poisson.simulate(100, beta0)
         self.assertEqual(X.shape, (100, 4))
         self.assertEqual(y.shape, (100,))
-        # try to recover params
-        from statsmodels.discrete.discrete_models import Poisson
+        # try to recover params using frequentist regression
         ml_fit = Poisson(y, X).fit()
-        assert np.linalg.norm(beta0 - ml_fit.params, 2) < 0.05
+        self.assertLess(np.linalg.norm(beta0 - ml_fit.params, 2), 0.05)
 
-    
+    def testVariationalPoisson(self):
+        beta0 = np.r_[1.1, 2.2, 3.3, 4.4]
+        y, X = poisson.simulate(100, beta0)
+        mu_0, C_0 = np.zeros(4), np.eye(4)
+        fit = poisson.poisson_vi_reg(y, X, mu_0, C_0)
+        self.assertIsInstance(fit, dict)
